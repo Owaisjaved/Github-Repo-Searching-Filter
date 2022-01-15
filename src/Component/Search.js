47c0dbc0eb9f3b2a259851dd2365c15364383be3
OwaisJaved <owaisjaved954@gmail.com>
2022-01-15 18:55:48 +0100

    Graphql Merge Complete

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -1,124 +1,188 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import {getData} from './Query'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 export const Search = () => {
+  /**
+   * @States
+   */
+  //User Search Value
+  const [search, setSearch] = useState("");
+  // User Profile Data
+  const [userdata, setUserData] = useState({});
+  // User Repository Data
+  const [repoData, setRepoData] = useState([]);
+  // Filtered Data from Searched Value
+  const [filterData, setFilterData] = useState([]);
+  // Data Loading State
+  const [loading, setLoading] = useState(true);
 
+  /**
+   * Changing Object Values will Populate Data Respectively
+   * @public
+   */
+  let userInfo = {
+    name: "OwaisJaved",
+    token: "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",
+  };
+
+  /** @function
+   * A Function that Fetches User Github Data using GraphQl
+   */
+  const getUserGithub = async () => {
+    let fetchedData = await axios.post(
+      "https://api.github.com/graphql",
+      {
+        // Query to Get Required Values
+        query: `{
+                 user(login: "${userInfo.name}") {
+                   id  
+                   name
+                   login
+                   avatarUrl
+                   location
+                   login
+                   bio
+                   followers {
+                     totalCount
+                   }
+                   following {
+                     totalCount
+                   }
+                   repositories(first: 50) {
+                     nodes {
+                       id
+                       name
+                       description
+                       languages(first: 10) {
+                         nodes {
+                           name
+                         }
+                       }
+                       updatedAt
+                     }
+                     pageInfo {
+                       hasNextPage
+                     }
+                   }
+                 }
+               }`,
+      },
+      {
+        headers: { Authorization: `Bearer ${userInfo.token}` },
+      }
+    );
+
+    // Setting Values For SetStates(userData)
+    setUserData({
+      id:fetchedData.data.data.user.id,
+      name: fetchedData.data.data.user.name,
+      avatar_url: fetchedData.data.data.user.avatarUrl,
+      login: fetchedData.data.data.user.login,
+      bio: fetchedData.data.data.user.bio,
+      followers: fetchedData.data.data.user.followers.totalCount,
+      following: fetchedData.data.data.user.following.totalCount,
+      location: fetchedData.data.data.user.location,
+    });
+    // Setting Values For SetStates(repoData)
+    console.log(fetchedData.data.data.user.repositories.nodes)
+    setRepoData(fetchedData.data.data.user.repositories.nodes);
+    // Data is Fetched So loading status change to False
+    setLoading(false);
+  };
+  /** @function
+   * A Function that Set values for States
+   * User Searched value is Set First then Search state is matched with Repository Data State
+   */
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
     /**
-     * The Varaiable is available to all.
-     * Changing this name will Populate Data Regarding to the User Defined
-     * @public
+     * Returns Searched Data
+     * Checking User Searched Value in User Repository Data
+     * @returns {array}
      */
-    var globalUserName = 'OwaisJaved'
+    let filterData = repoData.filter((item) =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilterData(filterData);
+  };
 
-    //** @States*/
-    //User Search Value
-    const [search, setSearch] = useState('')
-    // User Profile Data
-    const [userdata, setUserData] = useState({})
-    // User Repository Data
-    const [repoData, setRepoData] = useState([])
-    // Filtered Data from Searched Value
-    const [filterData, setFilterData] = useState([])
-    // Data Loading State
-    const [loading, setLoading] = useState(true)
+  // React UseEffect hook for Mounting Function
+  useEffect(() => {
+    getUserGithub();
+  }, []);
 
-    console.log(loading)
-    //** @function
-    // A Function that Fetches User Profile Data. */
-    const getUser = async () => {
-        let url = `https://api.github.com/users/${globalUserName}`
-        let fetchedData = await axios.get(url)
-        setUserData(fetchedData.data)
-    }
-    //** @function
-    // A Function that Fetches User Repositories Data & Set result into SetState. */
-    const getUserRepo = async () => {
-        let url = `https://api.github.com/users/${globalUserName}/repos`
-        let fetchedData = await axios.get(url)
-        setRepoData(fetchedData.data)
-        setLoading(false)
-    }
-    //** @function
-    // A Function that Set values for States. */
-    const handleSearch = (e) => {
-        setSearch(e.target.value)
-        /**
-         * Returns Searched Data
-         * Checking User Searched Value in User Repository Data
-         * @returns {array}
-         */
-        let filterData = repoData.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
-        setFilterData(filterData)
-    }
-
-    // React UseEffect hook for Mounting Functions
-    useEffect(() => {
-        getUser()
-        getUserRepo()
-    }, [])
-
-    return (
-        <div className='Main'>
-            <div className='Container'>
-                {/* Checking if Data is Available or Not */}
-                {loading ? <span>Loading...</span>
-                    :
-                    <div className='left-side'>
-                        <div className='user-image'><img src={userdata.avatar_url} alt='UserProfile' ></img></div>
-                        <div>
-                            <div className='bigText'>{userdata.name}</div>
-                            <div className='normalText'>{userdata.login}</div>
-                        </div>
-                        <div className='normalText'>{userdata.bio}</div>
-                        <div className='profile-btn'>Edit Profile</div>
-                        <div className='block'>
-                            <div className='smallText'>{userdata.followers} follower</div>
-                            <span className='smallText'>.</span>
-                            <div className='smallText'>{userdata.following} following</div>
-                        </div>
-                        <div>{userdata.location}</div>
-                    </div>
-                }
-                <div className='right-side'>
-                    <div className='repo-detail'>
-                        {repoData.length} Repositories
-                    </div>
-                    <div id='searching'>
-                        <input placeholder='Search Repo...' type={'search'} value={search} onChange={(e) => handleSearch(e)}></input>
-                    </div>
-                      {/* Checking if Data is Available or Not */}
-                    {
-                        loading ?
-                            <span> loading...</span>
-                            :
-                            // If User has Search for Something then This block will run
-                            search.length > 1 ?
-                                filterData.map(item => (
-                                    <div className='Repo-Cards' key={item.id}>
-                                        <div className='bigText' style={{ color: '#47A6FF' }}>{item.name}</div>
-                                        <div className='normalText'>{item.description}</div>
-                                        <div className='block'>
-                                            <div className=' smallText boxStyle' >{item.language}</div>
-                                            <div className='smallText' style={{ paddingLeft: '10px' }}>Updated On {new Date(item.updated_at).toDateString()}</div>
-                                        </div>
-                                    </div>
-                                ))
-                                :
-                                // Else This Block Will Run
-                                repoData?.map(item => (
-                                    <div className='Repo-Cards' key={item.id}>
-                                        <div className='bigText' style={{ color: '#47A6FF' }}>{item.name}</div>
-                                        <div className='normalText'>{item.description}</div>
-                                        <div className='block'>
-                                            <div className=' smallText boxStyle' >{item.language}</div>
-                                            <div className='smallText' style={{ paddingLeft: '10px' }}>Updated On {new Date(item.updated_at).toDateString()}</div>
-                                        </div>
-                                    </div>
-                                ))
-                    }
-
-                </div>
+  return (
+    <div className="Main">
+      <div className="Container">
+        {/* Checking if Data is Available or Not */}
+        {loading ? (
+          <span>Loading...</span>
+        ) : (
+          <div className="left-side">
+            <div className="user-image">
+              <img src={userdata.avatar_url} alt="UserProfile"></img>
             </div>
+            <div>
+              <div className="bigText">{userdata.name}</div>
+              <div className="normalText">{userdata.login}</div>
+            </div>
+            <div className="normalText">{userdata.bio}</div>
+            <div className="profile-btn">Edit Profile</div>
+            <div className="block">
+              <div className="smallText">{userdata.followers} follower</div>
+              <span className="smallText">.</span>
+              <div className="smallText">{userdata.following} following</div>
+            </div>
+            <div>{userdata.location}</div>
+          </div>
+        )}
+        <div className="right-side">
+          <div className="repo-detail">{repoData.length} Repositories</div>
+          <div id="searching">
+            <input
+              placeholder="Search Repo..."
+              type={"search"}
+              value={search}
+              onChange={(e) => handleSearch(e)}
+            ></input>
+          </div>
+          {/* Checking if Data is Available or Not */}
+          {loading ? (
+            <span> loading...</span>
+          ) : // If User has Search for Something then This block will run
+          search.length > 1 ? (
+            filterData.map((item) => (
+              <div className="Repo-Cards" key={item.id}>
+                <div className="bigText" style={{ color: "#47A6FF" }}>
+                  {item.name}
+                </div>
+                <div className="normalText">{item.description}</div>
+                <div className="block">
+                  <div className=" smallText boxStyle">{item.languages.nodes.map(item=>(<div key={Math.random()}>{item.name}</div>))}</div>
+                  <div className="smallText" style={{ paddingLeft: "10px" }}>
+                    Updated On {new Date(item.updatedAt).toDateString()}
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            // Else This Block Will Run
+            repoData?.map((item) => (
+              <div className="Repo-Cards" key={item.id}>
+                <div className="bigText" style={{ color: "#47A6FF" }}>
+                  {item.name}
+                </div>
+                <div className="normalText">{item.description}</div>
+                <div className="block">
+                  <div className=" smallText boxStyle">{item.languages.nodes.map(item=>(<div key={Math.random()}>{item.name}</div>))}</div>
+                  <div className="smallText" style={{ paddingLeft: "10px" }}>
+                    Updated On {new Date(item.updatedAt).toDateString()}
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
